5db566c9de5a4d075c8d5fef8513dfaccdb72508
Pauli Salmenrinne <susundberg@gmail.com>
2020-01-04 11:38:22 +0200

    Add conflict test

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -8,6 +8,7 @@ from pathlib import Path
 unique_name = common.unique_name
 
 
+    
 class Test(common.TestBase):
 
     def setUp(self):
@@ -17,7 +18,9 @@ class Test(common.TestBase):
 
     def tearDown(self):
         self.other.tempdir_clear()
-
+    
+    
+    
     def test_0_basic(self):
         self.repo.cmd("sync", "--dry-run", self.other.path, assume_fail=True)
         self.other.file_check("root_file_1", exists=False)
@@ -37,7 +40,19 @@ class Test(common.TestBase):
         self.repo.cmd("sync", self.other.path)
         self.other.file_check(un, exists=True)
         self.other.file_check("root_file_1", exists=False)
-
-
+    
+    def test_1_conflict( self ):
+        os.chdir(self.other.path)
+        self.other.cmd("init")
+        un = unique_name() + "/" + unique_name()
+        self.other.file_make( un )
+        self.other.cmd("save")
+        
+        os.chdir(self.repo.path)
+        self.repo.cmd("sync", "--dry-run", self.other.path, assume_fail=True)
+        self.repo.cmd("sync", self.other.path, assume_fail=True)
+        
+        
+   
 if __name__ == "__main__":
     unittest.main()
